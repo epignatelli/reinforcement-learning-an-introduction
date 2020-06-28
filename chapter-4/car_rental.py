e940e3b446f114a2f23df7b986f5e7461a3a3444
epignatelli <eduardo.pignatelli@burohappold.com>
2020-06-28 17:52:51 +0100

    jack's car rental

--- chapter-4/car_rental.py
+++ chapter-4/car_rental.py
@@ -9,15 +9,16 @@ from functools import partial
 import seaborn as sns
 import math
 
-ACTIONS = list(range(-5, 6))
+
+ACTIONS = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 CAR_RENTAL_COST = 10
 CAR_MOVE_COST = 2
 MAX_CARS = 20
 MAX_MOVE = 5
-REQUEST_A_LAMBDA = 3
-RETURN_A_LAMBDA = 3
-REQUEST_B_LAMBDA = 4
-RETURN_B_LAMBDA = 2
+REQUEST_1_LAMBDA = 3
+DROPOFF_1_LAMBDA = 3
+REQUEST_2_LAMBDA = 4
+DROPOFF_B_LAMBDA = 2
 DISCOUNT = 0.9
 
 
@@ -28,15 +29,15 @@ class CarRental:
 
     def reset(self):
         self._poisson_prob = {}
-        self._P1, self._R1 = self.precompute_model(REQUEST_A_LAMBDA, RETURN_A_LAMBDA)
-        self._P2, self._R2 = self.precompute_model(REQUEST_B_LAMBDA, RETURN_B_LAMBDA)
         self.state_values = np.zeros((MAX_CARS + 1, MAX_CARS + 1))
-        self.policy = np.zeros((MAX_CARS + 1, MAX_CARS + 1))
+        self.policy = np.zeros((MAX_CARS + 1, MAX_CARS + 1), np.int32)
+        self._probs_1, self._rewards_1 = self.precompute_model(REQUEST_1_LAMBDA, DROPOFF_1_LAMBDA)
+        self._probs_2, self._rewards_2 = self.precompute_model(REQUEST_2_LAMBDA, DROPOFF_B_LAMBDA)
         return
 
     def step(self, state, action):
-        morning_n1 = state[0] - action
-        morning_n2 = state[1] + action
+        morning_n1 = int(state[0] - action)
+        morning_n2 = int(state[1] + action)
         new_state = (morning_n1, morning_n2)
         reward = self.get_reward(new_state)
         return new_state, reward
@@ -44,27 +45,34 @@ class CarRental:
     def render(self):
         # plot value table
         fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-        sns.heatmap(self.state_values, ax=ax[0])
-        ax[0].set_ylim(0, 20)
+        sns.heatmap(self.state_values, cmap="gray", ax=ax[0])
+        ax[0].set_ylim(0, MAX_CARS)
         ax[0].set_title("Value table V_π")
         # plot policy
-        sns.heatmap(self.policy, vmin=-5, vmax=5, cmap="RdBu", ax=ax[1])
-        ax[1].set_ylim(0, 20)
+        cmaplist = [plt.cm.RdBu(i) for i in range(plt.cm.RdBu.N)]
+        dRbBu = matplotlib.colors.LinearSegmentedColormap.from_list('dRdBu', cmaplist, plt.cm.RdBu.N)
+        sns.heatmap(self.policy, vmin=-5, vmax=5, cmap=dRbBu, ax=ax[1], cbar_kws={"ticks": ACTIONS, "boundaries": ACTIONS})
+        ax[1].set_ylim(0, MAX_CARS)
         ax[1].set_title("Policy π")
         plt.show()
         return fig, ax
 
     def get_transition_probability(self, state, new_state):
-        return self._P1[(state[0], new_state[0])] * self._P2[(state[1], new_state[1])]
+        return self._probs_1[(state[0], new_state[0])] * self._probs_2[(state[1], new_state[1])]
 
     def get_reward(self, state):
-        return self._R1[state[0]] + self._R2[state[1]]
+        return self._rewards_1[state[0]] + self._rewards_2[state[1]]
 
     def get_valid_action(self, state, action):
         cars_at_1, cars_at_2 = state
+        # Jack can't move more cars than he has available
         action = max(-cars_at_2, min(action, cars_at_1))
+        # Jack can move at most 5 cars
         action = max(-MAX_MOVE, min(MAX_MOVE, action))
         return action
+
+    def get_available_actions(self, state):
+        return list(range(max(-MAX_CARS, - state[1]), min(MAX_CARS, state[0]) + 1))
 
     def poisson_probability(self, n, lam):
         key = (n, lam)
@@ -75,16 +83,16 @@ class CarRental:
     def precompute_model(self, lambda_requests, lambda_dropoffs):
         P, R = {}, {}
         requests = 0
-        for requests in range(26):
+        for requests in range(MAX_CARS + max(ACTIONS) + 1):
             request_prob = self.poisson_probability(requests, lambda_requests)
-            for n in range(27):
+            for n in range(MAX_CARS + max(ACTIONS) + 1):
                 if n not in R:
                     R[n] = 0.
                 R[n] += CAR_RENTAL_COST * request_prob * min(requests, n)
             dropoffs = 0
-            for dropoffs in range(26):
+            for dropoffs in range(MAX_CARS + max(ACTIONS) + 1):
                 dropoffs_prob = self.poisson_probability(dropoffs, lambda_dropoffs)
-                for n in range(26):
+                for n in range(MAX_CARS + max(ACTIONS) + 1):
                     satisfied_requests = min(requests, n)
                     new_n = max(0, min(MAX_CARS, n + dropoffs - satisfied_requests))
                     if (n, new_n) not in P:
@@ -94,26 +102,28 @@ class CarRental:
 
     def bellman_expectation(self, state, action):
         """
+        Solves the bellman expectation equation for given state
+        V(s) = p(s, r | s' π(s)) * (R(s) + γ * V(s'))
         Args:
             state (Tuple[int, int]): a tuple storing the number of available locations, respectively at A and B
             action (int): The key to the action dict
-            value (float): discount factor for future rewards
         Returns:
-            (float): the value of the current state pair
+            (float): the value V(s) of the current state pair
         """
         action = self.get_valid_action(state, action)
         (morning_n1, morning_n2), r = self.step(state, action)
 
-        state_value = -CAR_RENTAL_COST * abs(action)
-        for new_n1 in range(21):
-            for new_n2 in range(21):
+        state_value = -CAR_MOVE_COST * abs(action)
+        for new_n1 in range(MAX_CARS + 1):
+            for new_n2 in range(MAX_CARS + 1):
                 p = self.get_transition_probability((morning_n1, morning_n2), (new_n1, new_n2))
                 state_value += p * (r + DISCOUNT * self.state_values[new_n1, new_n2])
         return state_value
 
     def policy_evaluation(self, theta=1e-3):
         """
-        Computes the true value table for the current policy using iterative policy evaluation
+        Computes the true value table for the current policy using iterative policy evaluation.
+        At the end of the process it updates the state-value table with the newly computed value function.
         Args:
             env (CarRental): The CarRental environment, which contains the model dynamics, the active policy and the state-value table
         Returns:
@@ -125,52 +135,48 @@ class CarRental:
             for available_A in range(MAX_CARS + 1):
                 for available_B in range(MAX_CARS + 1):
                     state = (available_A, available_B)
+                    # a = π(s)
                     action = self.policy[available_A, available_B]
-                    # check for invalid actions:
-                    if state[0] - action > 20 or state[1] + action > 20:
-                        continue
-                    if state[0] < action:  # not enough cars in A to move
-                        continue
-                    if state[1] < -action:  # not enough cars in B to move
-                        continue
+                    # V(s) = p(s, r | s' π(s)) * (R(s) + γ * V(s'))
                     new_state_value = self.bellman_expectation(state, action)
                     # according the the original lisp code, the evaluation is performed asynchronously:
                     # http://incompleteideas.net/book/code/jacks.lisp
                     new_values[state] = new_state_value
             delta = np.max(np.abs(self.state_values - new_values))
-            print("\t\tValue delta {:.5f}\t".format(delta), end="\r")
-            if delta < theta:
-                return new_values
+            print("\t\tValue delta {:.5f}\t\t ".format(delta), end="\r")
             self.state_values = new_values.copy()
             new_values = np.empty_like(self.state_values)
+            if delta < theta:
+                print()
+                return
 
         raise ValueError("The value table did not converge. Check your inputs or look for bugs.")
 
     def policy_improvement(self):
         """
-        Makes one step of policy improvement using a greedy policy.
-        Args:
-            values (numpy.ndarray):
-            policy (numpy.ndarray):
-            discount (float):
+        Makes one step of policy improvement following a greedy policy.
+        For each state of the model, it iterates through all the feasible actions and finds the greediest one.
+        The current policy is updated synchronously for each state, i.e. only after all the states have been visited.
         Returns:
-            (numpy.ndarray): The improved policy
+            (bool): True if the policy has not improved
         """
         new_policy = np.empty_like(self.policy)
+        # for each state s ∈ S
         for available_A in range(MAX_CARS + 1):
             for available_B in range(MAX_CARS + 1):
                 state = (available_A, available_B)
-                best_value = self.bellman_expectation(state, self.policy[available_A, available_B])
-                best_action = self.policy[available_A, available_B]
-                for action in ACTIONS:
+                best_action = self.policy[state]
+                best_value = -float("inf")
+                for action in self.get_available_actions(state):
                     value = self.bellman_expectation(state, action)
+#                     print(state, action, value)
                     if value > best_value:
                         best_value = value
                         best_action = action
                 new_policy[available_A, available_B] = best_action
-        improved = (self.policy != new_policy).any()
-        self.policy = new_policy
-        return improved
+        converged = (new_policy == self.policy).all()
+        self.policy = new_policy.copy()
+        return converged
 
     def policy_iteration(self, plot=False):
         """
@@ -178,39 +184,38 @@ class CarRental:
         Convergence is guaranteed since the MDP has only a finite number of policies.
         Note that the optimal policy might not be unique.
         Args:
-            env (CarRental): The CarRental environment
-            policy (numpy.ndarray): an optional starting policy. If omitted the policy is initialised with zeros everywhere
+            plot (bool): If true, self.render() will be called at each evaluation/iteration step
         Returns:
             (numpy.ndarray): The optimal policy
         """
         # policy evaluation/improvement loop
         iteration = 1
+        if plot:
+            self.render()
         while True:
             # log
             print("Iterating through policy {}".format(iteration))
 
-            # policy evaluation to get the new value table
+            # policy evaluation to update the value table
             print("\tEvaluating policy {}".format(iteration))
-            new_values = self.policy_evaluation()
+            self.policy_evaluation()
 
-            # policy improvement to adjust the policy to the new value table
+            # policy improvement to update the current policy, based on the new value table
             print("\tImproving policy {}".format(iteration))
-            improved = self.policy_improvement()
+            converged = self.policy_improvement()
+
+            # has π converged to π*? I.e. is the current policy stable?
+            if converged:
+                print("Policy is stable. π converged to π*")
+                return
 
             # plot
             if plot:
                 self.render()
-
-            # has π converged to π*? I.e. is the current policy stable?
-            if not improved:
-                return
 
             iteration += 1
         raise ValueError("The policy did not converge. Check your inputs or look for bugs.")
 
 if __name__ == "__main__":
     env = CarRental()
-#     env.policy_evaluation()
-#     env.policy_improvement()
-#     env.render()
     env.policy_iteration(True)
